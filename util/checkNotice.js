35b02b052e89280a0f1a22d7066968f3525425a7
Squiddleton <mdlegomaster@gmail.com>
2023-01-18 13:33:00 -0500

    Add editServerStatus() and other minor changes

--- util/checkNotice.js
+++ util/checkNotice.js
@@ -1,22 +1,18 @@
 const Discord = require('discord.js');
-const { epicFetch } = require('../API/epicauth.js');
 
 /**
  *
  * @param {Discord.Client<true>} client
+ * @param {any[]} filteredNotices
  */
-module.exports = async function checkNotice(client) {
-	const json = await epicFetch('https://fortnitecontent-website-prod07.ol.epicgames.com/content/api/pages/fortnite-game?lang=en');
-	const notices = json.emergencynoticev2.emergencynotices.emergencynotices;
-
-	const embeds = notices.map(entry => {
-		return new Discord.EmbedBuilder()
-			.setAuthor({ name: 'Reina', iconURL: client.user.displayAvatarURL() })
-			.setTitle(`${entry.title}`)
-			.setColor('#FF0000')
-			.setDescription(entry.body)
-			.setImage('https://i.imgur.com/DF7QZbH.png');
-	});
+module.exports = async function checkNotice(client, filteredNotices) {
+	const embeds = filteredNotices.map(notice => new Discord.EmbedBuilder()
+		.setAuthor({ name: 'Reina', iconURL: client.user.displayAvatarURL() })
+		.setTitle(`${notice.title}`)
+		.setColor('#FF0000')
+		.setDescription(notice.body)
+		.setImage('https://i.imgur.com/DF7QZbH.png'),
+	);
 
 	['740559376070475796', '488040333310164992'].forEach(id => client.channels.cache.get(id).send({ embeds }));
 };
